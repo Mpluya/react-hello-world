ee924b5794018c01a1ca416fe3d7ffdf27a850f5
Mae Large <mlarge@vmware.com>
2022-02-14 07:17:40 -0700

    nudge

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Choreo</h1>
+      <h1>Hello Choreography</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,5 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  expect(screen.getByText('Hello Choreo')).toBeInTheDocument();
+  expect(screen.getByText('Hello Choreography')).toBeInTheDocument();
 });
